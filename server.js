b628d22be2cb7d1a3764bdcf3f5f028d697b0dde
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-04-29 11:09:20 -0300

    feat: create upload

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const editoraRoutes = require("./src/routes/editoraRoutes");
 const heroisRoutes = require("./src/routes/heroisRoutes");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", editoraRoutes)
 app.use("/api", heroisRoutes)
 
--- src/routes/heroisRoutes.js
+++ src/routes/heroisRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const heroisController = require("../controllers/heroisController");
+const upload = require("../config/upload.js");
 
 router.get("/herois", heroisController.getAllHerois);router.get("/herois/:id", heroisController.getHeroi);
-router.post("/herois", heroisController.createHeroi);
+router.post("/herois", upload.single("photo"),heroisController.createHeroi);
 router.put("/herois/:id", heroisController.updateHeroi);
 router.delete("/herois/:id", heroisController.deleteHeroi);
 
